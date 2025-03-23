569182e63f373102f6f290d5c85f45f75580cc5f
Manya-Js-mn <manyanotiya@gmail.com>
2025-03-23 13:24:15 +0530

    trying to deploy + final touches in login and signup

--- app.js
+++ app.js
@@ -16,11 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // Database Connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root", // Change if your MySQL has a password
-    database: "crop_login",
-    port: 3306
+    // host: "localhost",
+    // user: "root",
+    // password: "root", // Change if your MySQL has a password
+    // database: "crop_login",
+    // port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
